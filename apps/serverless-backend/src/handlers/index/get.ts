adc82f5f8c9c91f5f4686eb8f1d42158b584eabd
HollyGrahamPlank <hollygrahamplank@gmail.com>
2024-04-18 16:51:28 -0500

    Implemented serverless

---
+++ apps/serverless-backend/src/handlers/index/get.ts
@@ -0,0 +1,27 @@
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyHandler,
+  APIGatewayProxyResult,
+  Context,
+} from "aws-lambda";
+
+/**
+ * A Lambda Handler.
+ *
+ * @param event Info about the API Gateway event that triggered this lambda.
+ * @param context Context regarding this lambda call
+ * @returns APIGatewayProxyResult
+ */
+export const handler: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent,
+  context: Context,
+): Promise<APIGatewayProxyResult> => {
+  console.log("TODO");
+
+  return {
+    statusCode: 200,
+    body: "test",
+  };
+};
+
+export default handler;
